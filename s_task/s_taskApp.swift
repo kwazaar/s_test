d08934d351e8207abd1c4b93cf7122b868082aa3
kwazaar <maksim_sizov@icloud.com>
2024-01-11 19:55:41 +0300

    Initial Commit

---
+++ s_task/s_taskApp.swift
@@ -0,0 +1,17 @@
+//
+//  s_taskApp.swift
+//  s_task
+//
+//  Created by MaxOS on 11.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct s_taskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
